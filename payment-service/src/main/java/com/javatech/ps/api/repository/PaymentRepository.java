1d1acf405fb1569ff5b0ea498154cd6072cbae87
prashanthreddyjavatech <61052176+prashanthreddyjavatech@users.noreply.github.com>
2022-03-07 21:49:22 +0530

    microservices programs added

---
+++ payment-service/src/main/java/com/javatech/ps/api/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.javatech.ps.api.repository;
+
+import com.javatech.ps.api.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+    Payment findByOrderId(int orderId);
+}
+
